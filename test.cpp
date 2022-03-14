c51fc18657b45e9b98ad012b051061ed7c47e8dc
Criarino <richardfernandes11@yahoo.com.br>
2022-03-14 19:09:41 -0300

    adicionando comentarios

--- test.cpp
+++ test.cpp
@@ -17,12 +17,12 @@ int sans=Engine.AbrirFonte("Sans.ttf", 15);
 int main()
 {
 	Engine.CriarJanela(1280, 680, "Lorem Ipsum");
-	Engine.CriarObj(&testt, "iss.png", 0, 0, 100, 100);
-	Engine.CriarEntidade(&player, "player.png", 0, 0, 24, 32);
+	Engine.CriarObj(&testt, "iss.png", 0, 0, 100, 100);//criando um objeto de teste - um retangulo preto guardado em disco
+	Engine.CriarEntidade(&player, "player.png", 0, 0, 24, 32);//objeto com animação usando spritesheet
 	testt.setDest(340, 300);
-	corr=player.createCycle(3, 24, 32, 4, 4);
+	corr=player.createCycle(3, 24, 32, 4, 4);//registrando animações
 	idolr=player.createCycle(1, 24, 32, 2, 20);
-	player.setCurAnimation(idolr);
+	player.setCurAnimation(idolr);//setando animação
 	while(rodando)
 	{
 		Engine.SetBackground(126, 192, 238, 255);
@@ -58,7 +58,7 @@ void render()
 {
 	int x, y;
 	SDL_GetMouseState(&x, &y);
-	Engine.Desenhar(to_string(x)+','+to_string(y), sans, 3, 3, 255, 0, 0);
+	Engine.Desenhar(to_string(x)+','+to_string(y), sans, 3, 3, 255, 0, 0);//mostrar a posição x y do mouse no canto da tela
 	Engine.Desenhar(&player, 240, 240);
 	Engine.Desenhar(&testt);
 	Engine.Commit(60);
